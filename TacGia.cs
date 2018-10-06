6f19181cd50e5169e9c429107210f21416bca828
Tu Nguyen <miamor@users.noreply.github.com>
2018-10-06 20:04:14 +0700

    AuthorMenu

---
+++ TacGia.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QuanLy
+{
+    public partial class TacGia : Form
+    {
+        public TacGia()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+              
+    }
+}
